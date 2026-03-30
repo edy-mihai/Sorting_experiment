8c74246b599e66768f97b0ba77b1776029f4f294
edy-mihai <edypetco@gmail.com>
2026-03-30 19:39:44 +0300

    added the generator

--- gen.c
+++ gen.c
@@ -4,4 +4,32 @@
 
 int main()
 {
+    int n;
+    char filename[100];
+
+    printf("Numbers to generate: ");
+    scanf("%d", &n);
+
+    printf("File name: ");
+    scanf("%s", filename);
+
+    srand(time(NULL));
+
+    FILE *fp = fopen(filename, "w");
+    if (fp == NULL)
+    {
+        printf("Wrong file name. \n");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int random_nr = rand();
+        fprintf(fp, "%d\n", random_nr);
+    }
+
+    fclose(fp);
+    printf("Created file: %s.", filename);
+
+    return 0;
 }
